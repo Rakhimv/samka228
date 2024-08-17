dffb2b99f000d0be147ad98b1c6a1478ce5d377f
AziCode <111975081+Rakhimv@users.noreply.github.com>
2024-08-18 02:39:11 +0500

    Automated commit for deploying files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="flex flex-col gap-[10px] items-center">
+                <h1 className="text-center mt-[50px] text-[20px] font-mono text-white font-bold">test 228</h1>
+                <a className="text-white underline font-mono text-center" href="https://github.com/Rakhimv">GitHub</a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
